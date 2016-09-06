df30b208faf7325f90e7c894ac80bd69745eceed
Della Anjeh <dellaanjeh@Dellas-MacBook-Pro.local>
2016-09-05 21:41:51 -0700

    adds splash screen and icon

--- app/src/main/java/com/example/dellaanjeh/dootapp/SplashActivity.java
+++ app/src/main/java/com/example/dellaanjeh/dootapp/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.dellaanjeh.dootapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
